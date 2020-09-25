6f738f6bd142c0d64a65fd2302fbaded0962dc80
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-09-25 04:25:56 -0400

    Initial commit

---
+++ src/pageResponses.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+// the client.html file
+const index = fs.readFileSync(`${__dirname}/../pages/index.html`);
+// the style.css file
+const mainStyle = fs.readFileSync(`${__dirname}/../styles/mainStyles.css`);
+
+const respond = (request, response, status, content, contentType) => {
+  response.writeHead(status, { 'Content-Type': contentType });
+  response.write(content);
+  response.end();
+};
+
+// Serve the index page
+const getIndex = (request, response) => respond(request, response, 200, index, 'text/html');
+
+// Serve the style.css page
+const getMainStyle = (request, response) => respond(request, response, 200, mainStyle, 'text/css');
+
+// Respond to a request for a page that doesn't exist with a JSON response object.
+const notFound = (request, response) => {
+    const responseJSON = {
+      id: 'notFound',
+      message: 'The page requested was not found.',
+    };
+  
+    return respond(request, response, 404);
+  };
+
+module.exports = {
+  getIndex,
+  getMainStyle,
+  notFound,
+};
---
+++ src/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const pageResponses = require('./pageResponses.js');
+const comicResponses = require('./comicResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': pageResponses.getIndex,
+    '/index.html': pageResponses.getIndex,
+    '/mainStyle.css': pageResponses.getMainStyle,
+    notFound: pageResponses.notFound,
+  },
+};
+
+const handleGET = (request, response, parsedUrl) => {
+    if (urlStruct[request.method][parsedUrl.pathname]) {
+      urlStruct[request.method][parsedUrl.pathname](request, response);
+    } else {
+      urlStruct[request.method].notFound(request, response);
+    }
+  };
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
+
+  // If the request was a GET or HEAD request, handle it from URL struct
+  // Else handle with POST request to addUser
+  if (request.method === 'GET' || request.method === 'HEAD') handleGET(request, response, parsedUrl);
+
+  else handlePOST(request, response, parsedUrl);
+};
+
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
